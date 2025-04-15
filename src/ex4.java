990322b6763324696cac6ddb7fa1a5971cce7571
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-14 22:14:10 -0300

    Subindo os exercícios 4 e 5

---
+++ src/ex4.java
@@ -0,0 +1,32 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class ex4 {
+    public static void main(String[] args) {
+
+        DecimalFormat dc = new DecimalFormat("0.00");
+        Scanner teclado = new Scanner(System.in);
+
+    //variaveis
+        //*** x>=5 ou x<=-5
+        double x, y;
+
+     //entrada de dados
+
+        System.out.print("Valor de x: ");
+        x = teclado.nextDouble();
+     //
+
+        if (x>5||x<-5){
+            y = 8/(Math.sqrt(Math.pow(x,2)-25));
+            System.out.print("O valor de y é: "+ dc.format(y));
+
+                                                            }
+            else{
+            System.out.print("Digite um valor valido.");
+                                                            }
+
+
+
+    }
+}
---
+++ src/ex5.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ex5 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        double ladoA,ladoB,ladoC;
+
+        System.out.print("Digite o valor do primeiro lado: ");
+        ladoA = teclado.nextDouble();
+
+        System.out.print("Digite o valor do segundo lado: ");
+        ladoB = teclado.nextDouble();
+
+        System.out.print("Digite o valor do terceiro lado: ");
+        ladoC = teclado.nextDouble();
+
+        if (ladoA+ladoB>ladoC && ladoA+ladoC>ladoB && ladoC+ladoB>ladoA){
+            if (ladoA==ladoB && ladoA==ladoC){
+                System.out.print("Os valores formam um triângulo real equilatero");
+            }
+            else{
+            System.out.print("Os valores formam um triângulo real");
+            }
+        }
+
+        else {
+            System.out.print("Os Valores não formam um triangulo. Repita!");
+        }
+
+    }
+}
